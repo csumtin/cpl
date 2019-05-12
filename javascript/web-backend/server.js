673452ce577bf65fcdbf93882e19a07eab2ab896
csumtin <csumtin@protonmail.com>
2019-05-11 21:00:09 -0700

    Simple web-backend example

---
+++ javascript/web-backend/server.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const https = require("https");
+const fs = require("fs");
+
+const server = https.createServer({"key" : fs.readFileSync("privkey.pem"), "cert" : fs.readFileSync("fullchain.pem")}, function(request, response) {
+  console.log(request.method, request.url);
+
+  if(request.method === "POST") {
+    // check request.url?
+    getPostData().then(function(postData) {
+      // sample jsonrpc post
+      // curl -k -i -X POST -H 'Content-Type: application/json' -d '{"jsonrpc": "2.0", "id" : "id", "method" : "method", "params" : ["params"]}' https://127.0.0.1:8000
+      console.log(postData["id"], postData["method"], postData["params"]);
+      
+      const clientRequestID = generateRandomID();
+      response.writeHead(200, {"content-type": "application/json"});
+      response.end(JSON.stringify({"jsonrpc": "2.0", "id" : clientRequestID, "result" : "hello"}), "utf-8");
+    });
+  }
+  
+  function getPostData() {
+    return new Promise(function(resolve) {
+      let postData = "";
+      request.on("data", function(chunk) {
+        postData += chunk.toString();
+      });
+      request.on("end", function() {
+        resolve(JSON.parse(postData));
+      }); 
+    });
+  }
+  
+  // TODO crypto secure version
+  function generateRandomID() {
+    return Math.random().toString();
+  }
+});
+
+server.listen(8000, "0.0.0.0");
+console.log("Server running on 8000");
